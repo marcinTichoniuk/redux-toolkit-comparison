35a3b04a47613e909c69a5edb37c6cf33a416852
Marcin Tichoniuk <marcin.tichoniuk1@gmail.com>
2021-08-30 09:52:06 +0200

    Add 'logger' middleware to store

--- src/redux-toolkit/Store.ts
+++ src/redux-toolkit/Store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
 
 import { reducer } from "./RootReducer";
 
 export const store = configureStore({
   reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV !== "production",
 });
