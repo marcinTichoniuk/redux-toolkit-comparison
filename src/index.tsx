53e5cfef9843a7cbbd983b5546b5d9ad6602199f
Marcin Tichoniuk <marcin.tichoniuk1@gmail.com>
2021-08-30 09:26:28 +0200

    Connect store to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux-old-way/Store";
+
+// import { store } from "./redux-old-way/Store";
+import { store } from "./redux-toolkit/Store";
 
 import App from "./App";
 import "./index.css";
