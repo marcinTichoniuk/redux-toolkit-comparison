97cac80e6d897f60296ca904aacf27fbfe990f05
Marcin Tichoniuk <marcin.tichoniuk1@gmail.com>
2021-09-04 08:35:42 +0200

    Add action type in extra reducer

--- src/redux-toolkit/CounterSlice.ts
+++ src/redux-toolkit/CounterSlice.ts
@@ -13,9 +13,9 @@ export const counterSlice = createSlice({
   // },
   extraReducers: (builder) => {
     builder
-      .addCase(todosSlice.actions.create, (state) => state + 1)
-      .addCase(todosSlice.actions.edit, (state) => state + 1)
-      .addCase(todosSlice.actions.toggle, (state) => state + 1)
-      .addCase(todosSlice.actions.remove, (state) => state + 1);
+      .addCase(todosSlice.actions.create.type, (state) => state + 1)
+      .addCase(todosSlice.actions.edit.type, (state) => state + 1)
+      .addCase(todosSlice.actions.toggle.type, (state) => state + 1)
+      .addCase(todosSlice.actions.remove.type, (state) => state + 1);
   },
 });
